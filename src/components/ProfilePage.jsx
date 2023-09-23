d7b27a90574743ab4e96eb043ff7909fd7869696
EmunW <edmundpunch5@gmail.com>
2023-09-23 16:19:15 -0400

    Basic ProfilePage

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,5 +1,14 @@
-const ProfilePage = props => {
+import { Link } from 'react-router-dom'
 
+const ProfilePage = props => {
+  return(
+    <div>
+      <h1>Profile page</h1>
+      <Link className='link' to='/login'>
+        <h1>Logout</h1>
+      </Link>
+    </div>
+  )
 }
 
 export default ProfilePage;
