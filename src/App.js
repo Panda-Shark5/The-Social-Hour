bc9b0be188735947247e7d5787764efad440c6a7
EmunW <edmundpunch5@gmail.com>
2023-09-23 15:10:33 -0400

    setup app router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import SignupPage from './components/SignupPage'
+import LoginPage from './components/LoginPage'
+import FeedPage from './components/FeedPage'
+import ProfilePage from './components/ProfilePage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/login' element={<LoginPage/>}/>
+        <Route exact path='/profile' element={<ProfilePage/>}/>
+        <Route exact path='/feed' element={<FeedPage/>}/>
+        <Route exact path='/signup' element={<SignupPage/>}/>
+        <Route path='*' element={<Navigate to='/login' replace />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
