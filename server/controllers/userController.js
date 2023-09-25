aa476cf41e25cf4bf061a7b4fb6db958ae68cc2d
Pravek M. Karwe <pkarwe62@gmail.com>
2023-09-25 15:31:40 -0400

    adding auth workflow via formidable

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,6 @@
 const User = require('../models/UserModel');
+const { listen } = require('../server');
+const { formidable } = require('formidable');
 
 const UserController = {};
 
@@ -22,16 +24,32 @@ UserController.addUser = async (req, res, next) => {
 UserController.verifyUser = async (req, res, next) => {
 
     try {
-       
-        
-        const { username, password } = req.params;
-        console.log('inside verify user. req body is:', req.body)
-        console.log('inside verify user. req params is:', req.params)
-        console.log('inside verify user. req querey is:', req.query)
+
+        const form = formidable({});
+        let fields; let files;
+        [fields, files] = await form.parse(req);
+
+        let bodyStr = '';
+        req.on("data", function (chunk) {
+            bodyStr += chunk.toString();
+        });
+        req.on("end", function () {
+            console.log(bodyStr);
+        });
+
+        //console.log('testing', JSON.stringify({ fields, files }, null, 2));
+        console.log(fields['username']);
+        console.log(fields['password']);
+
+        const username = String(fields['username']);
+        const password = String(fields['password']);
+
+        console.log('username is', username);
+        console.log('password is', password);
         // console.log('inside verify user. credentials are', username, password)
         const user = await User.findOne({ username: username, password: password });
         res.locals.verifiedUser = user;
-        
+
         return next();
     } catch (err) {
         return next(err);
