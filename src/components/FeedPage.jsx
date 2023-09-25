b1850a51ae9c94df5c4139aa01e8234eb0e63b20
EmunW <edmundpunch5@gmail.com>
2023-09-25 10:04:47 -0400

    Basic feed page

--- src/components/FeedPage.jsx
+++ src/components/FeedPage.jsx
@@ -1,5 +1,14 @@
-const FeedPage = props => {
+import { Link } from 'react-router-dom'
 
+const FeedPage = props => {
+  return(
+    <div>
+      <h1>Feed page</h1>
+      <Link className='link' to='/login'>
+        <h1>Logout</h1>
+      </Link>
+    </div>
+  )
 }
 
 export default FeedPage;
