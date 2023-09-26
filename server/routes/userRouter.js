e4903070b820f7ddf7b6d75b6ff81dc60e212064
Robert Mayo <rbrtm984@gmail.com>
2023-09-26 17:51:21 -0400

    began SQL refactoring and changed sign up page to use SQL database hosted on elephantSQL

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../db');
 //const User = require('../models/UserModel');
 const userController = require('../controllers/userController');
 const cookieController = require('../controllers/cookieController')
 
 
-router.post('/register', userController.addUser, cookieController.setSSIDCookie, (req, res) => {
-    return res.status(200).redirect('http://localhost:3000/feed');
-});
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
 
-router.post('/login', userController.verifyUser, cookieController.setSSIDCookie, (req, res) => {
-    return res.status(200).redirect('http://localhost:3000/feed');
-});
+    try {
+        // Check for an existing user with the same username or email
+        const userExists = await db.oneOrNone('SELECT * FROM users WHERE username = $1', [username]);
 
-router.get('/:id', userController.getUser, (req, res) => {
-    return res.status(200).json(res.locals.user);
-});
+        if (userExists) {
+            return res.status(400).json({ message: 'Username or email is already in use.' });
+        }
 
+        // Insert the new user into the database
+        await db.none('INSERT INTO users (username, password) VALUES ($1, $2)', [username, password]);
+
+        // You can add additional logic here, such as setting cookies
+
+        return res.status(201).json({ message: 'Registration successful.' });
+    } catch (error) {
+        console.error('Error during registration:', error);
+        return res.status(500).json({ message: 'An error occurred during registration.' });
+    }
+})
 
 module.exports = router;
